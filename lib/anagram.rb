672a7cc3711582424e9cea793133b1853dbe20d0
jared frankel <jaredfrankel10@gmail.com>
2018-01-29 21:12:34 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,11 +4,11 @@ class Anagram
    def initialize(word)
      @word = word
    end
- 
+
    def match(candidates)
      candidates.select { |candidate| is_anagram?(candidate) }
    end
- 
+
    def is_anagram?(candidate)
     candidate.chars.sort == @word.chars.sort
    end
